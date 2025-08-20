8894c5a7bf7a25814882cd77d5d0ffc0aa1e6507
xiaoqi01010 <97711145+xiaoqi01010@users.noreply.github.com>
2025-08-20 23:40:48 +0800

    Added the function where Duke echoes command entered by user and exits when user types the command bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,16 @@ public class Duke {
     public static void main(String[] args) {
         String logo = "Sophia";
         System.out.println("Hello! I'm " + logo + "\nWhat can I do for you you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scn = new Scanner(System.in);
+        while(true) {
+            String input = scn.nextLine();
+            if(input.equals("bye")) {
+                scn.close();
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }else {
+                System.out.println(input);
+            }
+        }
     }
 }
