a4b3771fd8fa2878d458f4ac173a198808693930
xiaoqi01010 <97711145+xiaoqi01010@users.noreply.github.com>
2025-08-20 23:37:42 +0800

    Implemented a skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "Sophia";
+        System.out.println("Hello! I'm " + logo + "\nWhat can I do for you you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
