aacf0c807d0e7b03b7cd2bc96f8a0e898e217794
xiaoqi01010 <97711145+xiaoqi01010@users.noreply.github.com>
2025-08-21 18:44:12 +0800

    Added Parsers for each type of event to hide the process of command parsing. Added DukeExceptions to handle possible input errors

---
+++ src/main/java/EventTaskParser.java
@@ -0,0 +1,10 @@
+public class TodoTaskParser implements Parser {
+    String arguments;
+    public TodoTaskParser(String arguments) {
+        this.arguments = arguments;
+    }
+    @Override
+    public void parse() {
+
+    }
+}
---
+++ src/main/java/TodoTaskParser.java
@@ -0,0 +1,10 @@
+public class TodoEventParse implements Parser {
+    String arguments;
+    public TodoEventParse(String arguments) {
+        this.arguments = arguments;
+    }
+    @Override
+    public void parse() {
+
+    }
+}
