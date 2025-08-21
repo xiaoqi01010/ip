6f686dd1f3ed9cdab30b0421c9e666d12c567cde
xiaoqi01010 <97711145+xiaoqi01010@users.noreply.github.com>
2025-08-21 11:07:39 +0800

    added encapsulations to methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,17 +5,37 @@ public class Duke {
     //Keep a list of internal tasks
     private final List<Task> tasks = new ArrayList<>();
     private int count = 0;
-
-    //instantiate
-    private void run() throws InvalidIndexException {
-        String logo = "Sophia";
-        System.out.println("Hello! I'm " + logo + "\nWhat can I do for you you?");
+    private String name = null;
+    public Duke(String name) {
+        this.name = name;
+    }
+    private void printList(){
+        for(Task task : tasks) {
+            System.out.println(task.toString());
+        }
+    }
+    private void addTask(String input){
+        tasks.add(new Task(input,++count));
+    }
+    public void handleTask(int index, boolean done){
+        if (index < 0 || index >= tasks.size()) {
+            System.out.println("No such task exists!");
+            return;
+        }
+        tasks.get(index).setDone(done);
+        System.out.println((done ? "Nice! I've marked this task as done:\n"
+                : "OK, I've marked this task as not done yet:\n")
+                + tasks.get(index).toString()
+        );
+    }
+    private void run() {
+        System.out.println("Hello! I'm " + this.name + "\nWhat can I do for you you?");
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
             String input = scanner.nextLine().trim();
-            String[] user_inputs = input.split(" ");
-            String cmd = user_inputs[0].toLowerCase();
+            String[] userInputs = input.split("\\s+",2);
+            String cmd = userInputs[0].toLowerCase();
             switch(cmd) {
                 case "bye" -> {
                     scanner.close();
@@ -23,46 +43,33 @@ public class Duke {
                     return;
                 }
                 case "list" -> {
-                    for(Task task : tasks) {
-                        System.out.println(task.toString());
-                    }
+                    printList();
                     break;
                 }
                 case "mark","unmark" -> {
-                    if(user_inputs.length != 2) {
+                    if(userInputs.length != 2) {
                         System.out.println("Usage: "+cmd+" <index>");
                         break;
                     }
                     int index;
                     try{
-                        index = Integer.parseInt(user_inputs[1])-1;
-                    }catch(NumberFormatException e){
+                        index = Integer.parseInt(userInputs[1])-1;
+                    } catch(NumberFormatException e){
                         System.out.println("Index must be a number.");
                         break;
                     }
-                    if(index < 0 || index >= tasks.size()) {
-                        throw new InvalidIndexException("No such task exists!");
-                    }
                     boolean done = cmd.equals("mark");
-                    tasks.get(index).setDone(done);
-                    System.out.println((done ? "Nice! I've marked this task as done:\n"
-                            : "OK, I've marked this task as not done yet:\n")
-                            + tasks.get(index).toString()
-                    );
+                    handleTask(index,done);
                     break;
                 }
                 default -> {
-                    tasks.add(new Task(input,++count));
+                    addTask(input);
                     System.out.println("added: " + input);
                 }
             }
         }
     }
     public static void main(String[] args) {
-        try{
-            new Duke().run();
-        }catch(InvalidIndexException e){
-            System.out.println(e.getMessage());
-        }
+            new Duke("Sophia").run();
     }
 }
